870f3142fa43eb2b5e7947714557255467ecd58f
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-04-30 07:52:52 +0800

    Add echo Hello World endpoint

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", helloWorld)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func helloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World!")
+}
